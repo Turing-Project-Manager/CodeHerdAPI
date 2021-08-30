422f045bac62c1cbdd745a83d6cad02ae37909ce
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-30 16:59:39 -0700

    Add a test to ensure that it can get a project summary, create two base tests, pending

--- spec/graphql/queries/query_project_spec.rb
+++ spec/graphql/queries/query_project_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'query project', type: :request do
-  it 'returns project name information' do
-    user = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
-    project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103", owner_id: user.id)
-    post '/graphql', params: { query: "{ project(id: #{project.id}) {name} }" }
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    expect(parsed[:data][:project][:name]).to eq(project.name)
+  describe 'project information' do
+    xit 'can find all projects for a user' do
+
+    end
+
+    it 'returns project name information' do
+      user = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
+      project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103", owner_id: user.id)
+      post '/graphql', params: { query: "{ project(id: #{project.id}) {name} }" }
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed[:data][:project][:name]).to eq(project.name)
+    end
+
+    it 'returns project summary information' do
+      user = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
+      project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103", owner_id: user.id)
+      post '/graphql', params: { query: "{ project(id: #{project.id}) {summary} }" }
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed[:data][:project][:summary]).to eq(project.summary)
+    end
+
+    it 'returns project name information' do
+      user = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
+      project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103", owner_id: user.id)
+      post '/graphql', params: { query: "{ project(id: #{project.id}) {name} }" }
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed[:data][:project][:name]).to eq(project.name)
+    end
   end
 end
