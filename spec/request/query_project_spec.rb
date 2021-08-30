f2ea63a79c4bca13eb471fc4daff7064bcb43d79
Taija <taija.warbelow@gmail.com>
2021-08-30 16:50:26 -0700

    return project name information when querying a project, spec passing

--- spec/request/query_project_spec.rb
+++ spec/request/query_project_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'query project' do
+RSpec.describe 'query project', type: :request do
   it 'returns project name information' do
     user = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
-    project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103",  owner: user.id)
-
-    post '/graphql', params: { query: Project {name} }
+    project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103", owner_id: user.id)
+    post '/graphql', params: { query: "{ project(id: #{project.id}) {name} }" }
     parsed = JSON.parse(response.body, symbolize_names: true)
-    expect(parsed).to eq({ "data": { "name": project.name }})
+    expect(parsed[:data][:project][:name]).to eq(project.name)
   end
 end
