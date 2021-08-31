9a4e288c15c353f89732cd1c6a399300adc02189
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-30 17:05:21 -0700

    Create passing test for mod number

--- spec/graphql/queries/query_project_spec.rb
+++ spec/graphql/queries/query_project_spec.rb
@@ -22,12 +22,12 @@ RSpec.describe 'query project', type: :request do
       expect(parsed[:data][:project][:summary]).to eq(project.summary)
     end
 
-    it 'returns project name information' do
+    it 'returns project mod_number information' do
       user = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
       project = Project.create!(name: "My project", summary: "This is my project summary", mod_number: "2103", owner_id: user.id)
-      post '/graphql', params: { query: "{ project(id: #{project.id}) {name} }" }
+      post '/graphql', params: { query: "{ project(id: #{project.id}) {modNumber} }" }
       parsed = JSON.parse(response.body, symbolize_names: true)
-      expect(parsed[:data][:project][:name]).to eq(project.name)
+      expect(parsed[:data][:project][:modNumber]).to eq(project.mod_number)
     end
   end
 end
