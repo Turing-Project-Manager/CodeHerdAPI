85c63665e81f6932ed1114254421daecb9846313
Zachary Green <zachjamesgreen@gmail.com>
2021-09-01 08:54:09 -0600

    comment out uneeded class

--- app/graphql/types/queries/user_queries.rb
+++ app/graphql/types/queries/user_queries.rb
@@ -1,16 +1,16 @@
-module Queries
-  class User < Queries::BaseQuery
-    def allUsers
-      User.all
-    end
+# module Queries
+#   class User < Queries::BaseQuery
+#     def allUsers
+#       User.all
+#     end
 
-    field :user, Types::UserType, null: false do
-      description "return a user"
-      argument :id, ID, required: true
-    end
+#     field :user, Types::UserType, null: false do
+#       description "return a user"
+#       argument :id, ID, required: true
+#     end
 
-    def user(id:)
-      User.find(id)
-    end
-  end
-end
+#     def user(id:)
+#       User.find(id)
+#     end
+#   end
+# end
