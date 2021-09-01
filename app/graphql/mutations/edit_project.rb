2ab2e01411ca69dd272e4127aabd8ed1817c960f
Taija <taija.warbelow@gmail.com>
2021-08-31 20:52:46 -0700

    test and implement edit project name functionality
    remove unused defined variable
    
    remove unused defined variable
    
    move query method outside describe block, add type argument to allow reusing for name describe block.
    
    happy path spec for update project name, not passing
    
    add optional name argument to edit project mutation, spec passing
    
    comment out unused code

--- app/graphql/mutations/edit_project.rb
+++ app/graphql/mutations/edit_project.rb
@@ -3,19 +3,21 @@ module Mutations
     argument :user_id, ID, required: true
     argument :project_id, ID, required: true
     argument :summary, String, required: false
+    argument :name, String, required: false
 
     field :project, Types::ProjectType, null: true
     field :errors, [String], null: false
 
     def resolve(info)
       if Collaborator.where(user_id: info[:user_id], project_id: info[:project_id]).any?
-        user_id = info.delete(:user_id)
+        info.delete(:user_id)
         project_id = info.delete(:project_id)
         project = Project.find(project_id)
         if project.update(info)
           return_info(project, errors: [])
-        else
-          return_info(nil, errors: project.errors.full_messages)
+        # we currently do not have any sad paths or edge cases that use this else
+        # else
+        #   return_info(nil, errors: project.errors.full_messages)
         end
       else
         return_info(nil, errors: ["Please give a valid user id and project id."])
--- spec/request/mutations/edit_project_spec.rb
+++ spec/request/mutations/edit_project_spec.rb
@@ -1,32 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Edit Project', type: :request do
+  def query(user_id, project_id, type, content)
+    <<~GQL
+    mutation {
+      editProject(input:{
+        userId: #{user_id},
+        projectId: #{project_id},
+        #{type}: "#{content}"
+      }
+      ){
+        project{
+          id,
+          #{type}
+        }
+        errors
+      }
+    }
+    GQL
+  end
+
   describe 'update project summary' do
     it 'allows a collaborator to update the summary' do
       user_1 = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
       project_1 = Project.create!(name: "My project 1", summary: "This is my first project summary", mod_number: "3", owner_id: user_1.id)
-      collaboration_1 = Collaborator.create!(user_id: user_1.id, project_id: project_1.id )
+      Collaborator.create!(user_id: user_1.id, project_id: project_1.id )
       summary = "This is the new summary, yay!"
 
-      def query(user_id, project_id, summary)
-        <<~GQL
-        mutation {
-          editProject(input:{
-            userId: #{user_id},
-            projectId: #{project_id},
-            summary: "#{summary}"
-            }
-            ){
-              project{
-                id,
-                summary
-                }
-                }
-              }
-        GQL
-      end
-
-      post '/graphql', params: { query: query(user_1.id, project_1.id, summary) }
+      post '/graphql', params: { query: query(user_1.id, project_1.id, 'summary', summary) }
       parsed = JSON.parse(response.body, symbolize_names: true)
 
       expect(parsed[:data][:editProject][:project][:id]).to eq("#{project_1.id}")
@@ -36,32 +37,28 @@ RSpec.describe 'Edit Project', type: :request do
     it 'gives an error when the project does not exist' do
       user_1 = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
       project_1 = Project.create!(name: "My project 1", summary: "This is my first project summary", mod_number: "3", owner_id: user_1.id)
-      collaboration_1 = Collaborator.create!(user_id: user_1.id, project_id: project_1.id )
+      Collaborator.create!(user_id: user_1.id, project_id: project_1.id )
       summary = "This is the new summary, yay!"
 
-      def query(user_id, project_id, summary)
-        <<~GQL
-        mutation {
-          editProject(input:{
-            userId: #{user_id},
-            projectId: #{project_id},
-            summary: "#{summary}"
-            }
-            ){
-              project{
-                id,
-                summary
-                }
-                errors
-                }
-              }
-        GQL
-      end
-
-      post '/graphql', params: { query: query(user_1.id, "1387", summary) }
+      post '/graphql', params: { query: query(user_1.id, "1387", 'summary', summary) }
       parsed = JSON.parse(response.body, symbolize_names: true)
 
       expect(parsed[:data][:editProject][:errors][0]).to eq("Please give a valid user id and project id.")
+    end
+  end
+
+  describe 'update project name' do
+    it 'allows a collaborator to update the project name' do
+      user_1 = User.create!(name: "Turing Kiddo", email: "turing@turing.com", github_handle: "turing", github_access_token: "jkshfkjshauig47tyw49t8gzs9y")
+      project_1 = Project.create!(name: "My project 1", summary: "This is my first project summary", mod_number: "3", owner_id: user_1.id)
+      Collaborator.create!(user_id: user_1.id, project_id: project_1.id )
+      name = "The Great Pickling Project"
+
+      post '/graphql', params: { query: query(user_1.id, project_1.id, 'name', name) }
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed[:data][:editProject][:project][:id]).to eq("#{project_1.id}")
+      expect(parsed[:data][:editProject][:project][:name]).to eq(name)
     end
   end
 end
